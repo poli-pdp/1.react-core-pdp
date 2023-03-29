5d9d1d1938e61fcf01a265d1b267274b672d96e9
Giovanny Vargas <yovaz05@gmail.com>
2023-03-29 18:28:39 -0500

    clase 29-2023

---
+++ src/components/FormControlado.jsx
@@ -0,0 +1,14 @@
+const FormControlado = () => {
+	return (
+		<form>
+			<input
+				type='text'
+				id=''
+				name='email'
+				placeholder='Ingrese su Email'
+				autoComplete='off'
+			></input>
+		</form>
+	);
+};
+export default FormControlado;
